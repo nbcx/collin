db3bb48609a3603048d150a26bf4c0878350a79b
collin <collin@nb.cx>
2026-04-28 11:45:39 +0800

    add vercel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Vercel from './components/Vercel.vue'
 </script>
 
 <template>
+  <Vercel />
   <HelloWorld />
 </template>
---
+++ src/components/Vercel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Vue + Vercel Demo</h1>
+
+    <n-button @click="fetchData">调用 API</n-button>
+
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const data = ref('')
+
+async function fetchData() {
+  const res = await fetch('/api/hello')
+  const json = await res.json()
+  data.value = JSON.stringify(json, null, 2)
+}
+</script>
