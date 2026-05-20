98634c2ae38f381bb2a3b7757d8ae15368754a05
collin <collin@nb.cx>
2026-05-20 14:21:03 +0800

    添加 Bootstrap 5.3.0-alpha1，更新页面结构和样式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import Supabase from './components/Supabase.vue'
+import Index from './components/Index.vue'
 </script>
 
 <template>
-  <Supabase />
-  <HelloWorld />
+  <Index />
 </template>
